f60bfe8670aa3fbc8a86ac0e5f62d7312e57ceda
chacaa <santiagocasaslopez@gmail.com>
2026-01-30 13:45:35 -0300

    feat(chip): adds customization

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -5,17 +5,25 @@ import Text from './Text';
 import useTheme from '../hooks/useTheme';
 
 import { convertHexToRgba } from '../utils/uiUtils';
+import type { IconName } from './Icon';
+import Icon from './Icon';
 
 type Props = PressableProps & {
   text: string;
   size?: 'normal' | 'small';
   disabled?: boolean;
+  iconName?: IconName;
+  textColor?: string;
+  backgroundColor?: string;
 };
 
 export default function Chip({
   text,
   size = 'normal',
   disabled = false,
+  backgroundColor,
+  textColor,
+  iconName,
   ...rest
 }: Props) {
   const { theme, colors } = useTheme();
@@ -29,16 +37,25 @@ export default function Chip({
         style={[
           styles.container,
           {
-            backgroundColor: convertHexToRgba(colors.foreground, 0.08),
+            backgroundColor:
+              backgroundColor || convertHexToRgba(colors.foreground, 0.08),
             borderRadius: theme.roundness,
             height,
           },
           disabled && styles.disabled,
         ]}
       >
+        {iconName && (
+          <Icon
+            style={styles.icon}
+            name={iconName}
+            size={size === 'small' ? 12 : 14}
+            color={textColor || colors.foreground}
+          />
+        )}
         <Text
           variant={textVariant}
-          style={{ color: colors.foreground }}
+          style={{ color: textColor || colors.foreground }}
           numberOfLines={1}
         >
           {text}
@@ -53,6 +70,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 12,
+    flexDirection: 'row',
   },
+  icon: { marginEnd: 8 },
   disabled: { opacity: 0.3 },
 });
