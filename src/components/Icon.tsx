9344bdda6a48d4fabb701df27d05244b081dfa6f
pvicens <112896251+pvicensSpacedev@users.noreply.github.com>
2026-06-23 10:56:05 -0300

    fix fontweight override + add props to icon for strokewidth (#6)
    * fix fontweight override + add props to icon for strokewidth
    
    * chore: run linter

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,9 @@
 import type { StyleProp, ViewStyle } from 'react-native';
-import type { LucideIcon } from 'lucide-react-native';
+import type { LucideIcon, LucideProps } from 'lucide-react-native';
 import * as icons from 'lucide-react-native';
 
 import useTheme from '../hooks/useTheme';
 
-// Filtrar solo los iconos (excluyendo utilidades como createLucideIcon, etc.)
 type IconsModule = typeof icons;
 type IconKeys = {
   [K in keyof IconsModule]: IconsModule[K] extends LucideIcon ? K : never;
@@ -12,14 +11,18 @@ type IconKeys = {
 
 export type IconName = IconKeys;
 
-type Props = {
-  style?: StyleProp<ViewStyle>;
+type Props = Omit<LucideProps, 'style'> & {
   name: IconName;
-  size?: number;
-  color?: string;
+  style?: StyleProp<ViewStyle>;
 };
 
-export default function Icon({ name, size = 24, color, style }: Props) {
+export default function Icon({
+  name,
+  size = 24,
+  color,
+  style,
+  ...props
+}: Props) {
   const { colors } = useTheme();
 
   const IconComponent = icons[name] as LucideIcon;
@@ -29,6 +32,7 @@ export default function Icon({ name, size = 24, color, style }: Props) {
       size={size}
       color={color ?? colors.foreground}
       style={style}
+      {...props}
     />
   );
 }
