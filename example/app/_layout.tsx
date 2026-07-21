1c0d5e3a3ffe7d33dd2f16908d4e666843c36fd8
Chacaa <santiagocasaslopez@gmail.com>
2026-07-22 00:02:19 +0300

    fix: typescript warning

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout() {
             return;
           }
         }
-      } catch (e) {
+      } catch {
         // Ignore errors
       } finally {
         setRtlLoaded(true);
